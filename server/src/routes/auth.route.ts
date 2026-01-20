7bde9783b9f661e981812ab2f43d53f26b62953c
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2026-01-20 12:27:53 +0530

    created send otp function

--- server/src/routes/auth.route.ts
+++ server/src/routes/auth.route.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
-import {registerUser, loginUser} from "../controllers/auth.controller";
+import { registerUser, loginUser } from "../controllers/auth.controller";
+import { sendOtp } from "../controllers/otp.controller";
 
 const router = Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+
+router.post("/send-otp", sendOtp);
 
 export default router;

f3d2c1aef2e995ddae09f460b86f693d58e8b7a6
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2026-01-25 22:42:41 +0530

    minor fix in order route

--- server/src/routes/order.route.ts
+++ server/src/routes/order.route.ts
@@ -1,5 +1,8 @@
 import { Router } from "express";
+import { createOrder } from "../controllers/order.controller";
 
 const router = Router();
+
+router.post("/create/:tableId", createOrder); 
 
 export default router;

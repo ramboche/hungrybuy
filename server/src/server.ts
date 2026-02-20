f84841b89a22286e18d655b5aa2cbc01a9ab49dd
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2026-02-20 23:40:29 +0530

    fixed middleware for dev

--- server/src/server.ts
+++ server/src/server.ts
@@ -18,8 +18,10 @@ dotenv.config();
 export function startServer() {
   const app = express();
 
-  app.use(express.json({ limit: "10kb" }));
-  app.use(cors({ origin: [process.env.FRONTEND_URL!, process.env.ADMIN_URL!], credentials: true }));
+  // app.use(express.json({ limit: "10kb" }));
+  // app.use(cors({ origin: process.env.FRONTEND_URL!, credentials: true }));
+  app.use(express.json());
+  app.use(cors());
   app.use(httpLogger);
 
   app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
